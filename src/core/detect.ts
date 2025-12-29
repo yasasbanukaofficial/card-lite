64781ea013775e2d9321fd1acf9e010afba35877
Yasas Banuka <yixbinfo@gmail.com>
2025-12-30 00:27:28 +0530

    fix(detect): detect every type of card

--- src/core/detect.ts
+++ src/core/detect.ts
@@ -2,12 +2,22 @@ import { CardType, cardTypes } from "../types";
 
 export function detectCardType(cardNumber: string): CardType | undefined {
   for (const type of Object.values(cardTypes)) {
-    for (const patterns of type.patterns) {
-      const strPattern =
-        typeof patterns === "number"
-          ? patterns.toString()
-          : patterns[0].toString();
-      return cardNumber.startsWith(strPattern) ? type : undefined;
+    for (const pattern of type.patterns) {
+      if (Array.isArray(pattern)) {
+        const [min, max] = pattern;
+        const startDigits = parseInt(
+          cardNumber.substring(0, min.toString().length)
+        );
+        if (startDigits >= min && startDigits <= max) {
+          return type;
+        }
+      } else {
+        const strPattern = pattern.toString();
+        if (cardNumber.startsWith(strPattern)) {
+          return type;
+        }
+      }
     }
   }
+  return undefined;
 }

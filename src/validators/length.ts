3fa8748a7868f90aceaee2ad9ecc0143f44237fb
Yasas Banuka <yixbinfo@gmail.com>
2025-12-29 16:54:29 +0530

    feat(length): checks card validation by card number length

---
+++ src/validators/length.ts
@@ -0,0 +1,12 @@
+import { CardType, cardTypes } from "../types";
+
+export function validateLength(
+  cardNumber: string,
+  cardType?: CardType
+): boolean | undefined {
+  const cardLength = cardNumber.length;
+  if (cardType) return cardType.lengths.includes(cardLength);
+  return Object.values(cardTypes).some((type) =>
+    type.lengths.includes(cardLength)
+  );
+}
